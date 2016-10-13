b5642d72767d62de96d945527b03e74475532c6d
James Kim <james.kim7@yahoo.com>
2016-10-12 22:46:43 -0400

    ampm conditionals

--- clock.js
+++ clock.js
@@ -8,15 +8,23 @@ function timer() {
 	var seconds = document.getElementById("seconds");
 	var milliseconds = document.getElementById("milliseconds");
 	var currentDate = document.getElementById("current-date");
+	var amPm = document.getElementById("amPm");
 	
 	var time = {
 		"hours": date.getHours(),
 		"minutes": date.getMinutes(),
 		"seconds": date.getSeconds(),
-		"milliseconds": date.getMilliseconds()
+		"milliseconds": date.getMilliseconds(),
+		"amPm": "AM"
 	};
 
 	currentDate.innerHTML = date
+	
+	if (time.hours > 12) {
+		time.hours -= 12
+		time.amPm = "PM"
+		amPm.innerHTML = time.amPm
+	}
 
 	hours.innerHTML = time.hours < 10 ? "0" + time.hours : time.hours
 	minutes.innerHTML = time.minutes < 10 ? "0" + time.minutes : time.minutes
@@ -33,8 +41,6 @@ function timer() {
 	
 	setTimeout(timer, 102);
 }
-
-
 
 
 
